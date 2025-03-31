3e8c6852596e7fc5c77036fe4bfb030475a5d0ca
maicolviv695@gmail.com <maicolviv695@gmail.com>
2025-03-31 15:07:58 -0500

    refactor: use environment variables for database configuration

--- back/database.js
+++ back/database.js
@@ -1,10 +1,12 @@
 // database.js
 const mysql = require('mysql2');
+require('dotenv').config(); 
+
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '12345',
-    database: 'panel_solar'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 module.exports = connection;
